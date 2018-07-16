0d52ce2d62eb5cc63bc7db07f135d3c487dd74a0
Johnny Santos <johnnyadsantos@gmail.com>
2018-07-16 10:45:06 -0300

    Add editor and number click events

---
+++ BitcoinPOS-App/BitcoinPOS-App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BitcoinPOS_App.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private string resultText = string.Empty;
+
+        public string ResultText
+        {
+            get => resultText;
+            set { SetProperty(ref resultText, value); }
+        }
+    }
+}
--- BitcoinPOS-App/BitcoinPOS-App/Views/MainPage.xaml.cs
+++ BitcoinPOS-App/BitcoinPOS-App/Views/MainPage.xaml.cs
@@ -1,18 +1,43 @@
 ﻿using System;
 using Xamarin.Forms;
+using System.Diagnostics;
+using BitcoinPOS_App.ViewModels;
 
 namespace BitcoinPOS_App.Views
 {
     public partial class MainPage : ContentPage
     {
+        private readonly MainPageViewModel _viewModel;
+
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = _viewModel = new MainPageViewModel();
         }
 
         private async void Settings_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new SettingsPage());
+        }
+
+        private void Number_Clicked(object sender, EventArgs e)
+        {
+            var bt = sender as Button;
+
+            if (bt == null)
+                return;
+
+            Debug.WriteLine($"Botão apertado: {bt.AutomationId}");
+
+            if (short.TryParse(bt.AutomationId, out var num))
+            {
+                _viewModel.ResultText += num;
+            }
+            else
+            {
+                Debug.WriteLine($"Erro ao converter {nameof(bt)}.{nameof(bt.AutomationId)}");
+            }
         }
     }
 }
