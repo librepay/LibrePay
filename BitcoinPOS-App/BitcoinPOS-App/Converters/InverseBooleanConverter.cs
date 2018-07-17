e3583de34ffadd50970b8f0bedd4f03e691d70f2
Johnny Santos <johnnyadsantos@gmail.com>
2018-07-17 15:57:27 -0300

    Add converter to invert bool

---
+++ BitcoinPOS-App/BitcoinPOS-App/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace BitcoinPOS_App.Converters
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => InvertBool(value, targetType);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => InvertBool(value, targetType);
+
+        private static object InvertBool(object value, Type targetType)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool) value;
+        }
+    }
+}
