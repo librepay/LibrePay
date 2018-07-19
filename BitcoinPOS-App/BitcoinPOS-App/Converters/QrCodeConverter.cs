08d1fa9a468aa7c5a65812b7d83f8a39426053d0
Johnny Santos <johnnyadsantos@gmail.com>
2018-07-19 10:06:38 -0300

    QrCode Converter implementation

--- BitcoinPOS-App/BitcoinPOS-App/Converters/QrCodeConverter.cs
+++ BitcoinPOS-App/BitcoinPOS-App/Converters/QrCodeConverter.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Globalization;
+using System.IO;
+using BitcoinPOS_App.Models;
+using QRCoder;
 using Xamarin.Forms;
 
 namespace BitcoinPOS_App.Converters
@@ -8,7 +11,27 @@ namespace BitcoinPOS_App.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value == null || value.GetType() != typeof(Payment))
+                return null;
+
+            var payment = (Payment) value;
+
+            var generator = new PayloadGenerator.BitcoinAddress(
+                payment.Address
+                , System.Convert.ToDouble(payment.Value)
+            );
+            var payload = generator.ToString();
+
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.H);
+            var qrCode = new PngByteQRCode(qrCodeData);
+            var qrCodeAsPngArray = qrCode.GetGraphic(20);
+
+            // this memory stream should not be disposed here 
+            var ms = new MemoryStream(qrCodeAsPngArray);
+            qrCodeAsPngArray = null;
+
+            return ImageSource.FromStream(() => ms);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
