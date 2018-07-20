fa5af3239acc0ad1e0dc7d8eafb9f421e570191a
Johnny Santos <johnnyadsantos@gmail.com>
2018-07-20 09:47:27 -0300

    Add entropy

--- BitcoinPOS-App/BitcoinPOS-App/App.xaml.cs
+++ BitcoinPOS-App/BitcoinPOS-App/App.xaml.cs
@@ -1,31 +1,38 @@
+using System;
 using BitcoinPOS_App.Views;
+using NBitcoin;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
 namespace BitcoinPOS_App
 {
     public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // Adds entropy to the random utility used by NBitcoin
+            RandomUtils.AddEntropy($"bitcoin-pos-app-{DateTime.Now.Ticks}-{Device.RuntimePlatform}");
+
             MainPage = new NavigationPage(new MainPage());
-		}
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
